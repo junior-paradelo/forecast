af27ae41ba0c65050c5621450dcbaa11055eeebe
Junior Paradelo <juniorparadelo@gmail.com>
2020-07-10 18:53:57 +0200

    Uso de SpringBoot y pruebas con webjars

---
+++ rainforecast/src/main/java/es/udc/rainforecast/RainforecastApplication.java
@@ -0,0 +1,13 @@
+package es.udc.rainforecast;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RainforecastApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RainforecastApplication.class, args);
+	}
+
+}
---
+++ rainforecast/src/test/java/es/udc/rainforecast/RainforecastApplicationTests.java
@@ -0,0 +1,13 @@
+package es.udc.rainforecast;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RainforecastApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
